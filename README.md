c256f745d7d162ea98f813aa4818e8d6bcf71d2c
BrandonArgel <brandargel@gmail.com>
2022-03-29 19:33:16 -0600

    :bug: [UPDATE] src of audio and images

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 A project of design and creation of the frontend of a music player.
 With HTML, CSS and JS
 
-[Link to the project](https://brandonargel.github.io/music_player/)
+[Link to the project](https://brandonargel.github.io/music-player/)
--- js/main.js
+++ js/main.js
@@ -12,7 +12,15 @@ const close = document.querySelector(".close");
 const volume = document.getElementById("volume");
 
 // Song titles
-const songs = ["Dear God", "A little Piece Of Heaven", "Seize The Day", "Afterlife", "MIA", "So Far Away", "Until The End"];
+const songs = [
+	"Dear God",
+	"A little Piece Of Heaven",
+	"Seize The Day",
+	"Afterlife",
+	"MIA",
+	"So Far Away",
+	"Until The End",
+];
 
 // Keep track of songs
 let songIndex = 0;
@@ -22,70 +30,70 @@ loadSong(songs[songIndex]);
 
 // Update song details
 function loadSong(song) {
-  title.innerText = `Avenged Sevenfold - ${song}`;
-  audio.src = `/music_player/assets/audio/${song}.m4a`;
-  cover.src = `/music_player/assets/img/${song}.jpg`;
+	title.innerText = `Avenged Sevenfold - ${song}`;
+	audio.src = `/music-player/assets/audio/${song}.m4a`;
+	cover.src = `/music-player/assets/img/${song}.jpg`;
+  console.log(audio.src);
 }
 
 //Event listeners
 var boton = document.getElementById("boton");
 
 const updateProgress = () => {
-  if (audio.currentTime > 0) {
-    let barra = document.getElementById("progress");
-    barra.value = (audio.currentTime / audio.duration) * 100;
+	if (audio.currentTime > 0) {
+		let barra = document.getElementById("progress");
+		barra.value = (audio.currentTime / audio.duration) * 100;
 
+		var actualSegundos = audio.currentTime.toFixed(0);
+		actual = secondsToString(actualSegundos);
+		var duracionSegundos = audio.duration.toFixed(0) - audio.currentTime.toFixed(0);
+		dura = secondsToString(duracionSegundos);
 
-    var actualSegundos = audio.currentTime.toFixed(0);
-    actual = secondsToString(actualSegundos);
-    var duracionSegundos = audio.duration.toFixed(0) - audio.currentTime.toFixed(0);
-    dura = secondsToString(duracionSegundos);
+		document.getElementById("start").innerText = actual;
+		document.getElementById("end").innerText = dura;
+	}
 
-    document.getElementById("start").innerText = actual;
-    document.getElementById("end").innerText = dura;
-  }
+	if (audio.ended && bucleBtn.classList.contains("btn-active")) {
+		audio.currentTime = 0;
+		playSong();
+	}
 
-  if (audio.ended && bucleBtn.classList.contains("btn-active")) {
-    audio.currentTime = 0;
-    playSong();
-  }
+	if (audio.ended && shiftBtn.classList.contains("btn-active")) {
+		shiftSong();
+	}
 
-  if (audio.ended && shiftBtn.classList.contains("btn-active")) {
-    shiftSong();
-  }
-
-  if (
-    audio.ended &&
-    !bucleBtn.classList.contains("btn-active") &&
-    !shiftBtn.classList.contains("btn-active")
-  ) {
-    nextSong();
-  }
+	if (
+		audio.ended &&
+		!bucleBtn.classList.contains("btn-active") &&
+		!shiftBtn.classList.contains("btn-active")
+	) {
+		nextSong();
+	}
 };
 
 // Play & Pause button
 boton.addEventListener("click", function () {
-  if (audio.paused) {
-    playSong();
-  } else {
-    pauseSong();
-  }
+	if (audio.paused) {
+		playSong();
+	} else {
+		pauseSong();
+	}
 });
 
 function playSong() {
-  // The play method is asynchronous, the setTimeout helps to avoid an error
-  setTimeout(function () {
-    audio.play();
-  }, 0);
+	// The play method is asynchronous, the setTimeout helps to avoid an error
+	setTimeout(function () {
+		audio.play();
+	}, 0);
 
-  document.getElementById("play-pause").classList.add("button-play-pause");
-  document.getElementById("play-pause").classList.remove("play-pause");
+	document.getElementById("play-pause").classList.add("button-play-pause");
+	document.getElementById("play-pause").classList.remove("play-pause");
 }
 
 function pauseSong() {
-  audio.pause();
-  document.getElementById("play-pause").classList.remove("button-play-pause");
-  document.getElementById("play-pause").classList.add("play-pause");
+	audio.pause();
+	document.getElementById("play-pause").classList.remove("button-play-pause");
+	document.getElementById("play-pause").classList.add("play-pause");
 }
 
 // Change song events
@@ -93,109 +101,105 @@ prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
 
 function prevSong() {
-  songIndex--;
-  if (songIndex < 0) {
-    songIndex = songs.length - 1;
-  }
-  loadSong(songs[songIndex]);
-  playSong();
+	songIndex--;
+	if (songIndex < 0) {
+		songIndex = songs.length - 1;
+	}
+	loadSong(songs[songIndex]);
+	playSong();
 }
 
 function nextSong() {
-  if (shiftBtn.classList.contains("btn-active")) {
-    shiftSong();
-  }
+	if (shiftBtn.classList.contains("btn-active")) {
+		shiftSong();
+	}
 
-  songIndex++;
-  if (songIndex > songs.length - 1) {
-    songIndex = 0;
-  }
-  loadSong(songs[songIndex]);
-  document.getElementById("play-pause").classList.toggle("button-play-pause");
-  playSong();
+	songIndex++;
+	if (songIndex > songs.length - 1) {
+		songIndex = 0;
+	}
+	loadSong(songs[songIndex]);
+	document.getElementById("play-pause").classList.toggle("button-play-pause");
+	playSong();
 }
 
 // Shift songs
 function shiftSong() {
-  function numeroAleatorio(min, max) {
-    let numero = Math.round(Math.random() * (max - min) + min);
-    // console.log(numero);
-    // Funciona, número aleatorio entre 0 y 2 😁
-    function newMixSong(n) {
-      songIndex = n;
-      loadSong(songs[songIndex]);
-      document
-        .getElementById("play-pause")
-        .classList.toggle("button-play-pause");
-      playSong();
-      console.log(`Canción número: ${songIndex} del array`);
-    }
-    newMixSong(numero);
-  }
-  numeroAleatorio(0, songs.length - 1);
+	function numeroAleatorio(min, max) {
+		let numero = Math.round(Math.random() * (max - min) + min);
+		function newMixSong(n) {
+			songIndex = n;
+			loadSong(songs[songIndex]);
+			document.getElementById("play-pause").classList.toggle("button-play-pause");
+			playSong();
+			console.log(`Canción número: ${songIndex} del array`);
+		}
+		newMixSong(numero);
+	}
+	numeroAleatorio(0, songs.length - 1);
 }
 
 // Bucle button
 bucleBtn.addEventListener("click", bucle);
 function bucle() {
-  bucleBtn.classList.toggle("btn-active");
+	bucleBtn.classList.toggle("btn-active");
 }
 
 // Shift button
 shiftBtn.addEventListener("click", shift);
 function shift() {
-  shiftBtn.classList.toggle("btn-active");
+	shiftBtn.classList.toggle("btn-active");
 }
 
 // Convert seconds to string
 function secondsToString(seconds) {
-  var hour = "";
-  if (seconds > 3600) {
-    hour = Math.floor(seconds / 3600);
-    hour = hour < 10 ? "0" + hour : hour;
-    hour += ":";
-  }
-  var minute = Math.floor((seconds / 60) % 60);
-  minute = minute < 10 ? "0" + minute : minute;
-  var second = seconds % 60;
-  second = second < 10 ? "0" + second : second;
-  return hour + minute + ":" + second;
+	var hour = "";
+	if (seconds > 3600) {
+		hour = Math.floor(seconds / 3600);
+		hour = hour < 10 ? "0" + hour : hour;
+		hour += ":";
+	}
+	var minute = Math.floor((seconds / 60) % 60);
+	minute = minute < 10 ? "0" + minute : minute;
+	var second = seconds % 60;
+	second = second < 10 ? "0" + second : second;
+	return hour + minute + ":" + second;
 }
 
 // Progress bar
 progress.addEventListener("click", adelantar);
 function adelantar(e) {
-  const scrubTime = (e.offsetX / progress.offsetWidth) * audio.duration;
-  audio.currentTime = scrubTime;
-  // console.log(e);
+	const scrubTime = (e.offsetX / progress.offsetWidth) * audio.duration;
+	audio.currentTime = scrubTime;
+	// console.log(e);
 }
 
 // Drop down side panel
 config.addEventListener(
-  "click",
-  () => {
-    setTimeout(() => {
-      aside.classList.toggle("aside-toggle");
-      main.classList.toggle("filter");
-    });
-  },
-  0
+	"click",
+	() => {
+		setTimeout(() => {
+			aside.classList.toggle("aside-toggle");
+			main.classList.toggle("filter");
+		});
+	},
+	0
 );
 
 close.addEventListener("click", () => {
-  aside.classList.toggle("aside-toggle");
-  main.classList.toggle("filter");
+	aside.classList.toggle("aside-toggle");
+	main.classList.toggle("filter");
 });
 
 main.addEventListener("click", () => {
-  if (aside.classList.contains("aside-toggle")) {
-    aside.classList.remove("aside-toggle");
-    main.classList.remove("filter");
-  }
+	if (aside.classList.contains("aside-toggle")) {
+		aside.classList.remove("aside-toggle");
+		main.classList.remove("filter");
+	}
 });
 
 // Volume
 volume.oninput = (e) => {
-  const vol = e.target.value;
-  audio.volume = vol;
+	const vol = e.target.value;
+	audio.volume = vol;
 };
